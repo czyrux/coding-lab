bb788b39b68c3abf404a9ccb82bdb8490d9875fa
Antonio Gutierrez Martinez <a.gutierrez.martinez@zalando.de>
2015-08-19 15:30:18 +0200

    Simplify Adapter code

--- app/src/main/java/de/czyrux/codinglab/ui/CountryAdapter.java
+++ app/src/main/java/de/czyrux/codinglab/ui/CountryAdapter.java
@@ -1,32 +1,25 @@
 package de.czyrux.codinglab.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import android.widget.BaseAdapter;
 import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import de.czyrux.codinglab.R;
 import de.czyrux.codinglab.domain.Country;
 
-
 public class CountryAdapter extends BaseAdapter {
 
-    private final List<Country> countries;
+    private final List<Country> countries = new ArrayList<>(20);
 
-    public CountryAdapter() {
-        this.countries = new ArrayList<>(20);
-    }
+    public CountryAdapter() { }
 
-    public void addCountries(List<Country> countries) {
-        this.countries.addAll(countries);
-        this.notifyDataSetChanged();
-    }
-
-    public void setCountries(List<Country> countries) {
+    public void setCountries(final List<Country> countries) {
         this.countries.clear();
         this.countries.addAll(countries);
         this.notifyDataSetChanged();
@@ -38,40 +31,28 @@ public class CountryAdapter extends BaseAdapter {
     }
 
     @Override
-    public Object getItem(int position) {
+    public Object getItem(final int position) {
         return countries.get(position);
     }
 
     @Override
-    public long getItemId(int position) {
+    public long getItemId(final int position) {
         return position;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, final ViewGroup parent) {
 
         Country country = (Country) getItem(position);
 
-        CountryViewHolder viewHolder;
+        // A cause of Android recycling views on adapters, this view may be initialized already.
         if (convertView == null) {
             convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.country_list_item, parent, false);
-            viewHolder = new CountryViewHolder(convertView);
-            convertView.setTag(viewHolder);
-        } else {
-            viewHolder = (CountryViewHolder) convertView.getTag();
         }
 
-        viewHolder.name.setText(country.getName());
+        TextView nameTextView = (TextView) parent.findViewById(R.id.country_item_name);
+        nameTextView.setText(country.getName());
 
         return convertView;
-    }
-
-
-    static class CountryViewHolder {
-        TextView name;
-
-        CountryViewHolder(View parent) {
-            name = (TextView) parent.findViewById(R.id.country_item_name);
-        }
     }
 }
